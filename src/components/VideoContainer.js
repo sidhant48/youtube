118af32258fadd6789d40e829a98db2bc56abb9f
sidhant48 <sidhant.b48@gmail.com>
2025-02-15 10:50:26 +0530

    Initial commit

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,5 +1,38 @@
+// import React, { useEffect, useState } from "react";
+// import { YOUTUBE_VIDEOS_API } from "../utils/contants";
+// import VideoCard, { AdVideoCard } from "./VideoCard";
+// import { Link } from "react-router-dom";
+
+// const VideoContainer = () => {
+//   const [videos, setVideos] = useState([]);
+
+//   useEffect(() => {
+//     getVideos();
+//   }, []);
+
+//   const getVideos = async () => {
+//     const data = await fetch(YOUTUBE_VIDEOS_API);
+//     const json = await data.json();
+//     console.log("video container json:", json);
+//     setVideos(json.items);
+//   };
+
+//   return (
+//     <div className="flex flex-wrap">
+//       {videos[0] && <AdVideoCard info={videos[0]} />}
+//       {videos.map((video) => (
+//         <Link key={video.id} to={"/watch?v=" + video.id}>
+//           <VideoCard info={video} />
+//         </Link>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default VideoContainer;
+
 import React, { useEffect, useState } from "react";
-import { YOUTUBE_VIDEOS_API } from "../utils/contants";
+import { GOOGLE_API_KEY, YOUTUBE_VIDEOS_API } from "../utils/contants";
 import VideoCard, { AdVideoCard } from "./VideoCard";
 import { Link } from "react-router-dom";
 
@@ -11,9 +44,25 @@ const VideoContainer = () => {
   }, []);
 
   const getVideos = async () => {
-    const data = await fetch(YOUTUBE_VIDEOS_API);
-    const json = await data.json();
-    setVideos(json.items);
+    try {
+      const response = await fetch(YOUTUBE_VIDEOS_API, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${GOOGLE_API_KEY}`, // Replace with your access token
+          Accept: "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const json = await response.json();
+      console.log("video container json:", json);
+      setVideos(json.items);
+    } catch (error) {
+      console.error("Fetching videos failed:", error);
+    }
   };
 
   return (
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,9 +1,9 @@
-const GOOGLE_API_KEY = "AIzaSyAtIibgU4boIyzkgbabBCe8BJJsBSfNlWA";
+export const GOOGLE_API_KEY = "AIzaSyDRfGEmf6zhmGyyh9El8V3eIYnCqKbMjdg";
 
 export const LIVE_CHAT_COUNT = 25;
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=US&key=" +
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
